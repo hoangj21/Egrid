053748a07b438e545cc44b7296a75c6b29c8165a
Joanna Hoang <hoangj21@up.edu>
2019-02-16 21:44:14 -0800

    Data exploration for project proposal
    Added dashboard mockup and proposal files as well

--- Data_Exploration.r
+++ Data_Exploration.r
@@ -1,3 +1,4 @@
+#Load in data and print out summaries 
 emissions = read.csv("state_output_emissions.csv",header=T, na.strings = "?")
 emissions <- na.omit(emissions)
 fix(emissions)
@@ -17,3 +18,36 @@ egrid = read.csv("egrid_summary.csv",header=T, na.strings = "?")
 egrid <- na.omit(egrid)
 fix(egrid)
 summary(egrid)
+
+state = read.csv("state.csv",header=T, na.strings = "?")
+state <- na.omit(state)
+fix(state)
+summary(state)
+
+#viewing correlations between various variables
+cor(state$CO2,state$Oil)
+cor(state$CO2,state$Coal)
+cor(state$CO2,state$Gas)
+cor(state$CO2,state$Other.Fossil)
+cor(state$CO2,state$Nuclear)
+cor(state$CO2,state$Biomass)
+cor(state$CO2,state$Hydro)
+cor(state$CO2,state$Geo..thermal)
+cor(state$CO2,state$Solar)
+
+#had issues creating linear model, processing to remove some stray commas in data
+state$CO2 <- gsub(",", "", state$CO2)   # remove comma
+state$CO2 <- as.numeric(state$CO2)  
+par(mfrow=c(2,2))
+model=lm(CO2~Coal, data=state) 
+plot(model)
+
+#covariance,histograms, and means
+cov(state$CO2, state$Coal)
+hist(state$CO2)
+hist(state$Coal)
+mean(state$CO2)
+mean(state$Coal)
+
+
+
