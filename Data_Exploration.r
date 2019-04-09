9e3206f4e2bcc89711314742255bad59721dd99e
Joanna Hoang <hoangj21@up.edu>
2019-04-09 13:25:39 -0700

    Did some data preprocessing

--- Data_Exploration.r
+++ Data_Exploration.r
@@ -24,6 +24,24 @@ state <- na.omit(state)
 fix(state)
 summary(state)
 
+#Removing commas from data
+state$CO2 <- gsub(",", "", state$CO2)
+state$CH4 <- gsub(",", "", state$CH4)
+state$N2O<- gsub(",", "", state$N2O)
+state$CO2e <- gsub(",", "", state$CO2e)
+state$Annual.NOx <- gsub(",", "", state.Annual.NOx)
+state$Ozone.Season.NOx <- gsub(",", "", state$Ozone.Season.NOx)
+state$SO2 <- gsub(",", "", state$SO2)
+
+#changing emmissions to numeric form so that R functions work properly
+state$CO2 <- as.numeric(state$CO2)
+state$CH4 <- as.numeric(state$CH4)
+state$N2O <- as.numeric(state$N2O)
+state$CO2e <- as.numeric(state$CO2e)
+state$Annual.NOx <- as.numeric(state$Annual.NOx)
+state$Ozone.Season.NOx <- as.numeric(state$Ozone.Season.NOx)
+state$SO2 <- as.numeric(state$SO2)
+
 #viewing correlations between various variables
 cor(state$CO2,state$Oil)
 cor(state$CO2,state$Coal)
@@ -35,13 +53,28 @@ cor(state$CO2,state$Hydro)
 cor(state$CO2,state$Geo..thermal)
 cor(state$CO2,state$Solar)
 
+emmissions = c(state$CO2, state$CH4,state$N2O, state$CO2e,state$Annual.NOx, state$Ozone.Season.NOx,state$SO2)
+energy = c(state$Coal, state$Oil, state$Gas, state$Gas, state$Other.Fossil, state$Nuclear,state$Hydro, state$Biomass, state$Wind, state$Solar, state$Geo..thermal, state$Other.unknown..purchased.fuel)
+for(i in emmissions){
+  for(j in energy){
+    cor(i,j)
+  }
+  
+}
+for(i in 1:length(emmissions)){
+  for(j in 1:length(energy)){
+    print(energy[j])
+  }
+  print(emmissions[i])
+}
+
+
 #had issues creating linear model, processing to remove some stray commas in data
 state$CO2 <- gsub(",", "", state$CO2)   # remove comma
-state$CO2 <- as.numeric(state$CO2)  
 par(mfrow=c(2,2))
 model=lm(CO2~Coal, data=state) 
 plot(model)
-
+plot(state$CO2, state$Coal)
 #covariance,histograms, and means
 cov(state$CO2, state$Coal)
 hist(state$CO2)
