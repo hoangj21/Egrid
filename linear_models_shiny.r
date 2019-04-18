3decd168a26ee6d4471e345f0c91c6ae38df1955
Joanna Hoang <hoangj21@up.edu>
2019-04-18 15:41:47 -0700

    Added more comments

--- linear_models_shiny.r
+++ linear_models_shiny.r
@@ -13,11 +13,12 @@ library(magrittr)
 library(leaflet)
 library(geojsonio)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
   pageWithSidebar(
     headerPanel('Emissions vs. Energy Resource'),
     
+    #selection menus for linear graphs
     sidebarPanel(
       selectInput('xcol', 'Energy Resource', list("Coal", "Oil", "Gas", "Other.Fossil", "Nuclear", "Hydro", "Biomass", "Wind","Solar","Geothermal","Unknown"), "Coal"),
       selectInput('ycol', 'Emissions (lb/MWh)', list("CO2","CH4", "N2O", "CO2e","Annual.NOx","Ozone.Season.NOx","SO2"),"CO2")
@@ -25,7 +26,7 @@ ui <- fluidPage(
     
     mainPanel(
       fluidRow(
-        column(10, plotOutput('distPlot')),
+        column(10, plotOutput('linearModel')),
         column(10,  leafletOutput("mymap"))
       )
       
@@ -33,15 +34,17 @@ ui <- fluidPage(
   )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
+  source("Data_Preprocessing.r")
    
-   output$distPlot <- renderPlot({
-     source("Data_Preprocessing.r")
+   output$linearModel <- renderPlot({
+    
+     
+     #get strings of which resource or emission the user selects
      x = input$xcol
      y = input$ycol
-     
-     
+    
      #Array of emmissions data
      emmissions = cbind(state$CO2, state$CH4,state$N2O, state$CO2e,state$Annual.NOx, state$Ozone.Season.NOx,state$SO2)
      #Array for Energy source data
@@ -51,10 +54,12 @@ server <- function(input, output) {
      #Labels for energy sources
      energy.lab = c("Coal", "Oil", "Gas", "Other.Fossil", "Nuclear", "Hydro", "Biomass", "Wind","Solar","Geothermal","Unknown")
      
+     #Find indices of user's selections in the label arrays
      xIndex = match(x,energy.lab)
      yIndex = match(y,emmissions.lab)
      
-     plot(energy[,xIndex],emmissions[,yIndex],xlab=x, ylab=y,  col = "blue", pch = 15)
+     #Use indices to select correct data, and plot it
+     plot(energy[,xIndex],emmissions[,yIndex],xlab=x, ylab=y,  col = "red", pch = 15)
      model=lm(emmissions[,yIndex]~energy[,xIndex]) 
      abline(model)
      
@@ -62,17 +67,23 @@ server <- function(input, output) {
    
    output$mymap <- renderLeaflet({
      # From http://leafletjs.com/examples/choropleth/us-states.js
+     
+     #get json file for US map
      states <- geojsonio::geojson_read(x = "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json", what = "sp")
      
-     print(states$name)
+     #determine the range for the bins 
      bins <- c(0, 250, 500, 750, 1000, 1250, 1500, 1750,2000,Inf)
-     pal <- colorBin("Reds", domain = state$CO2, bins = bins)
      
+     #Set color palette 
+     pal <- colorBin(Yl, domain = state$CO2, bins = bins)
+     
+     #state and emissions labels
      labels <- sprintf(
        "<strong>%s</strong><br/>%g lb / MWH",
        state$State, state$CO2
      ) %>% lapply(htmltools::HTML)
      
+     #leaflet that draws map 
      m<- leaflet(states) %>%
        setView(-96, 37.8, 4) %>%
        addProviderTiles("MapBox", options = providerTileOptions(
